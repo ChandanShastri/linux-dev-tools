874033f37d8127c90f8d32bbc809c68588591965
Chandan Shastri <chandanshastri@outlook.com>
2024-03-15 22:33:41 +0530

    Adding shell name

--- shell_scripts/password-generator.sh
+++ shell_scripts/password-generator.sh
@@ -1,4 +1,7 @@
-#!/bin/bash
+#!/usr/bin/sh
 # Generate a 8 character length password.
+
+echo -e "\nDefault shell is $SHELL \n"
+
 read -p "Enter the length of the password :" length
 openssl rand -hex $length
