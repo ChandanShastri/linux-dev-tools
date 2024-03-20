8864f0ed8063889d23a065f55de44ec4238ecf32
Chandan Shastri <chandanshastri@outlook.com>
2024-03-20 21:58:17 +0530

    Adding comments

--- shell_scripts/password-generator.sh
+++ shell_scripts/password-generator.sh
@@ -5,4 +5,7 @@
 echo -e "\nDefault shell is $SHELL \n"
 
 read -p "Enter the length of the password :" length
-openssl rand -hex $((length / 2))
+password=$(openssl rand -hex $((length / 2)))
+
+echo -e "New Password : $password" 
+
